53a1072982db24d73dc32313102f211e4c01db63
Saule <talantbekkyzysaule6@gmail.com>
2023-05-24 14:47:04 +0600

    homework 3 lesson

---
+++ app.js
@@ -0,0 +1,21 @@
+// let userLogin = 'talantbekkyzysaule6@gmail.com'; 
+
+// let login = prompt('enter login')
+
+// if (userLogin === login) {
+//     alert ('WELCOME');
+// }
+// else {
+//     alert ('LOGIN INCORRECT')
+// }
+
+
+
+
+// let login = 'saule2002';
+
+// let password = 2002;
+// //          true        &&         true
+// if (login === 'saule2002' && password === 2002 ) {
+//    alert ('Welcome Saule')
+// };
